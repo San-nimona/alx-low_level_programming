eccca748dc37acfdf0a33b07607f8ed8753f5747
= <=>
2023-03-16 08:31:57 -0700

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ * Return: value 0
+*/
+
+int main(void)
+{
+        printf("Size of a char: %ld byte(s)\n", sizeof(char));
+        printf("Size of an int: %d byte(s)\n", sizeof(int));
+        printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+        printf("Size of an float: %ld byte(s)\n", sizeof(float));
+        return (0);
+}
