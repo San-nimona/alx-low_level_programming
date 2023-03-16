08302f5951c92140eebe839bedce8e044e7f3311
= <=>
2023-03-16 06:47:32 -0700

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ * Return: value 0
+*/
+
+int main(void)
+{
+	char str [] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
+}
