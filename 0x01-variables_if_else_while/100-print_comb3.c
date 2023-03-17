ddd3765dd394383b855601371bd7d46a39e069e0
= <=>
2023-03-17 12:25:33 -0700

    Last one

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Description: prints numbers
+ * main - Entyr point
+ * Description: prints two digits combination
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i, j;
+	int c, i;
 
-	for (i = '0'; i <= '9' ; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (j = '1'; j < '9'; j++)
+		for (i = '0'; i <= '9'; i++)
 		{
-			putchar(i);
-			putchar(j);
-			putchar(',');
-			putchar(' ');
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
