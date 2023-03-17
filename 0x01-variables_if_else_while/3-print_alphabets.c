6c83c626c6574cddef0246d895ec054e21ff83e2
= <=>
2023-03-17 09:07:44 -0700

    C fun

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpaBET
+ * i: for loop
+ * j: another loop
+ * Return: value 0
+ */
+
+int main(void)
+{
+	int i, j;
+	
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (j = 65; j < 91; j++)
+		putchar(j);
+	putchar(10);
+	return (0);
+}
