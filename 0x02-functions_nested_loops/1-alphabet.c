e8737fee888beb14e1d5b0b5ec1793f3baf96ed2
San-nimona <kaybryanmc@gmail.com>
2023-03-21 09:39:03 -0500

    CISFUN

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @i: takes values
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		_putchar(i);
+	_putchar(10);
+}
