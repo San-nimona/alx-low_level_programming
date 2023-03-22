6e17eb4213847d465fd74bc9bfd98a0bb06af708
= <=>
2023-03-22 15:29:26 -0700

    test

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,17 +2,17 @@
 
 /**
  * positive_or_negative - says if it's positive or negative
- * n: takes rnd no.
+ * @i: takes no.
+ *
  * Return: value 0
 */
 
 void positive_or_negative(int i)
 {
-        /* your code goes there */
-        if (i > 0)
-                printf("%d is positive\n", i);
-        else if (i < 0)
-                printf("%d is negative\n", i);
-        else
-                printf("%d is zero\n", i);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 }
