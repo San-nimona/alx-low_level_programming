d28b85a9032c6b60f9362e59b9d525fe3626cc52
= <=>
2023-03-23 07:38:53 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 nos
+ * @a: value 1
+ * @b: value 2
+ * result holds results
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
