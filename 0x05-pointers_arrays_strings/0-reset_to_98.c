47dc9e1682d4aa9ea79108648fa62f9beabc41ba
= <=>
2023-03-27 05:08:55 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - the function takes a number and resets it to 98
+ *
+ *@n: takes the value to be reset
+ */
+void reset_to_98(int *n)
+{
+	int *p;
+	
+	p = n;
+	*p = 98;
+}
