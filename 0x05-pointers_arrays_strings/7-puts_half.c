5020986d584cf85829f106db0f02b81afee1aca5
= <=>
2023-03-27 15:32:04 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - this prints the last half of string
+ * @str: the string
+ *
+ */
+void puts_half(char *str)
+{
+	int n;
+	int len = 0;
+
+	for (n = 0; str[n] != '\0'; n++)
+		len++;
+	if (len % 2 == 0)
+	{
+		for (n = len / 2; n <= len; n++)
+			printf("%c", str[n]);
+	}
+	else
+	{
+		for (n = (len - 1) / 2; n <= len; n++)
+			printf("%c", str[n]);
+	}
+	printf("\n");
+}
