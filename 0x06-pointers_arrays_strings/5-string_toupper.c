cb0e3fac07469444ece23b75249d9a865e3a3042
= <=>
2023-03-30 15:43:22 -0700

    Changing to all caps

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - A function that changes the string
+ *		    to all CAPS
+ *
+ * @s: pointer to int array
+ *
+ *
+ * Return: a pointer to string
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) <= 'z' && *(s + i) >= 'a')
+			*(s + i) -= 32;
+	}
+	return ((s + i));
+}
