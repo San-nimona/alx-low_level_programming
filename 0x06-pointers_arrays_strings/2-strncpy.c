5ccb12b5086a793c91b384eabe47c5f31abc47c1
= <=>
2023-03-30 13:54:21 -0700

    strcpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,10 +9,11 @@
  */
 
 char *_strncpy(char *dest, char *src, int n)
-{	
+{
 	int len_src = 0;
 	int i;
 	int len_dest = 0;
+
 	for (i = 0; (src[i] != '\0' && i < n); i++)
 	{
 		dest[len_dest] = src[len_src];
