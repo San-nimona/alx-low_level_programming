d99e326138ab164d169750c994d6de7883a8087e
San-nimona <kaybryanmc@gmail.com>
2023-04-03 16:15:06 -0500

    advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets a string to a char
+ * @s: the string to be changed
+ * @to: value of the pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
