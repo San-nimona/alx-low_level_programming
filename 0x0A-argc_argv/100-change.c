fc6f4812b9b2c8186a48ff98b75fb6ea816769d9
San-nimona <kaybryanmc@gmail.com>
2023-04-09 16:13:31 -0700

    last task requirement fixing

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,27 +2,14 @@
 #include <stdlib.h>
 
 /**
- * main - prints change to give
- * @argc: arg count
- * @argv: arg vector
- * Return: 0 or 1
+ * calc_coins - calculates the number of coins
+ * @cent: the amount input
+ * Return: success
  */
-
-int main(int argc, char **argv)
+int calc_coins(int cent)
 {
-	int cent, coins = 0;
+	int coins = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	cent = atoi(*(argv + 1));
-	if (cent < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
 	while (cent > 0)
 	{
 		if (cent >= 25)
@@ -54,4 +41,28 @@ int main(int argc, char **argv)
 	printf("%d\n", coins);
 	return (0);
 }
+/**
+ * main - prints change to give
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 or 1
+ */
 
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(*(argv + 1));
+	if (i < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	calc_coins(i);
+	return (0);
+}
