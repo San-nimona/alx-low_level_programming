bf0fbe3801b29c6bfee22f2b525abf6f330825ab
San-nimona <kaybryanmc@gmail.com>
2023-04-09 15:22:07 -0700

    last task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints change to give
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 or 1
+ */
+
+int main(int argc, char **argv)
+{
+	int cent, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cent = atoi(*(argv + 1));
+	if (cent < 0)
+		printf("0\n");
+	while (cent > 0)
+	{
+		if (cent >= 25)
+		{
+			cent -= 25;
+			coins++;
+		}
+		else if (cent >= 10)
+		{
+			cent -= 10;
+			coins++;
+		}
+		else if (cent >= 5)
+		{
+			cent -= 5;
+			coins++;
+		}
+		else if (cent >= 2)
+		{
+			cent -= 2;
+			coins++;
+		}
+		else
+		{
+			cent -= 1;
+			coins++;
+		}
+	}
+	printf("%d\n", coins);
+		return (0);
+}
+
