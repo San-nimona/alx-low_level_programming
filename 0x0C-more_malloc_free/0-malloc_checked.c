7f54925a21057535e9386a5fc6336b2f45463953
San-nimona <kaybryanmc@gmail.com>
2023-04-13 04:18:44 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Returns a pointer to the allocated memory
+ * @b: the memory size
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+0x0C-more_malloc_free
+More tasks about dynamic allocation of memory
