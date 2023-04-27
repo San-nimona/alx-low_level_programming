86b9ccd3a43a2392099d4d30c8500750759064fd
San-nimona <kaybryanmc@gmail.com>
2023-04-27 15:36:20 -0700

    fixing 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,8 +2,9 @@
 /**
  * print_first - prints before main
  */
-void __attribute__((constructor))print_first(void)
+void __attribute__((constructor))print_first(void);
+void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
