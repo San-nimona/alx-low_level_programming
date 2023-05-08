7d098c2f3b366dea4d9725aeec49cf5c50017d52
San-nimona <kaybryanmc@gmail.com>
2023-05-08 13:59:29 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, n, i;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	n = strlen(text_content);
 	i = write(fd, text_content, n);
 	if (i == -1)
