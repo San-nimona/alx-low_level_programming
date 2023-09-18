dbbd1682e7322aa1f24d38de1475bfdf1d662fc8
San-nimona <kaybryanmc@gmail.com>
2023-09-18 05:07:39 -0700

    task 2

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - adds int
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts 2 ints
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: dividend
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
